18cb4795338b133eff58314af2f2e976faef539b
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2024-01-25 23:07:50 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import AddTodoForm from './components/addTodoForm'
+import TodoList from './components/todolist'
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-md h-screen py-6">
+      <div className="flex flex-col p-4 border rounded-lg shadow-lg h-full">
+        <h1 className='text-2xl font-bold mb-4'>
+          Todo List
+        </h1>
+    <AddTodoForm/>
+    <TodoList/>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
